d41d2529032f1c7c9fa76d8741e96cfa8f5f734f
kamy <130361402+KamyEsm@users.noreply.github.com>
2026-09-21 15:22:17 +0330

    ini project

---
+++ src/test/java/com/kamyesm/bitsystembackend/BitSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamyesm.bitsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
